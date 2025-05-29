f44191f47a772386b208c669f787d4d379f5913c
edaraujo <ejrabt1@gmail.com>
2025-05-29 15:37:34 -0300

    feat: setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreditsPage from "./pages/CreditsPage";
+import GamePage from "./pages/GamePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/credits" element={<CreditsPage />} />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
